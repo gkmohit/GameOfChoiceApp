88189f192dd236f7ffeeff05fbdedda1cf22d514
gkmohit <gkmohit@gmail.com>
2015-07-26 22:36:07 -0400

    Added Story Activity.

--- app/src/main/java/com/gkmohit/unknown/gameofchoice/MainActivity.java
+++ app/src/main/java/com/gkmohit/unknown/gameofchoice/MainActivity.java
@@ -1,37 +1,46 @@
 package com.gkmohit.unknown.gameofchoice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private EditText mNameField;
+    private Button mStartButton;
+    private static final String NO_NAME_ENTERED = "Please enter a name!";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                if(name.matches("")){
+                    Toast.makeText(MainActivity.this, MainActivity.NO_NAME_ENTERED, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    String toastMessage = "HELLO " + name.toUpperCase() + "!!";
+                    Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+                    startStory();
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void startStory(){
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
